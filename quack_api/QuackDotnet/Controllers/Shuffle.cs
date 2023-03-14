77cdaafbbaed21242e123fb6411ee6166e694bcb
hop <avinashseelall@gmail.com>
2023-03-14 01:41:01 -0400

    changed-mcs

--- quack_api/QuackDotnet/Controllers/Shuffle.cs
+++ quack_api/QuackDotnet/Controllers/Shuffle.cs
@@ -6,14 +6,15 @@ namespace Quack.Controllers;
 [Route("[controller]")]
 public class ShuffleController : ControllerBase
 {
-    
+
     private string[]? Answers;
     [HttpPost(Name = "GetShuffle")]
     public Shuffle post(Shuffle bruh)
     {
         Console.WriteLine(bruh.Answer);
-    var rnd = new Random();
-        return new Shuffle {
+        var rnd = new Random();
+        return new Shuffle
+        {
             Answer = bruh.Answer.OrderBy(item => rnd.Next()).ToList()
         };
 
@@ -24,14 +25,73 @@ public class ShuffleController : ControllerBase
 [Route("[controller]")]
 public class CoinFlipController : ControllerBase
 {
-    
+
     private object[]? Answers;
     [HttpGet(Name = "GetCoinFlip")]
     public string get()
     {
-    var rnd = new Random();
-        return new[] {"h","t"}[rnd.Next(2)];
-        
+        var rnd = new Random();
+        return new[] { "h", "t" }[rnd.Next(2)];
+
+
+    }
+}
+
+
+public class WCC
+{
+    public Dictionary<string, List<string>>? data { get; set; }
+};
+
+
+[ApiController]
+[Route("[controller]")]
+public class WccController : ControllerBase
+{
+    [HttpPost(Name = "PostWcc")]
+    public object post(WCC data)
+    {
+        Dictionary<string, Dictionary<string, Dictionary<string, int>>> wc = new Dictionary<string, Dictionary<string, Dictionary<string, int>>>();
+        List<string> keyList = new List<string>(data.data.Keys);
+        foreach (string person in keyList)
+        {
+            Console.WriteLine(person);
+            foreach (string i2 in data.data[person])
+            {
+                string[] sentence = i2.Split(' ');
+                for (int i = 0; i < sentence.GetLength(0) - 1; i++)
+                {
+                    string word = sentence[i].ToLower();
+                    if (i < sentence.GetLength(0) - 1 && word != "")
+                    {
+                        if (wc.Keys.Contains(person))
+                        {
+                            if (wc[person].Keys.Contains(word))
+                            {
+                                if (wc[person][word].Keys.Contains(sentence[i + 1]))
+                                {
+                                    wc[person][word][sentence[i + 1]]++;
+                                }
+                                else
+                                {
+                                    wc[person][word].Add(sentence[i + 1], 1);
+                                }
+                            }
+                            else
+                            {
+                                var d = new Dictionary<string, int>() { { sentence[i + 1], 1 } };
+                                wc[person].Add(word, d);
+                            }
+                        }
+                        else
+                        {
+                            wc.Add(person, new Dictionary<string, Dictionary<string, int>>() { { word, new Dictionary<string, int>() { { sentence[i + 1], 1 } } } });
+                        }
+                    }
+                }
+            }
+        }
+        return wc;
 
     }
 }
